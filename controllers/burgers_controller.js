62efdd0431e0c2d72a0f03bac394ffb93a7aefb5
unknown <jodimcarlson4@gmail.com>
2018-12-28 09:14:52 -0600

    added more code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,10 +4,24 @@ var burgers = require("../models/burgers.js");
 
 router.get("/", function (request, response) {
     burgers.selectAll(function (data) {
-         console.log(data);
-         response.json(data);
+        console.log(data);
+        response.json(data);
     });
 });
+router.post("/burgers", function (request, response) {
+    burgers.insertOne(function (response) {
+        console.log(response);
+        response.json(response);
+    });
+});
+
+router.put("/burgers/:id", function (request, response) {
+    burgers.updateOne(function (response) {
+        console.log(response);
+        response.json(response);
+    });
+});
+
 
 
 

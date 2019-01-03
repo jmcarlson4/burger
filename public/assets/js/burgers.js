28caf07d083c711432e0631390da63e9630fc2ef
unknown <jodimcarlson4@gmail.com>
2019-01-02 18:22:00 -0600

    worked on code

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,22 +1,42 @@
-
-
 $(function () {
     $(".burger").on("click", function (event) {
         var id = $(this).data("id");
-        var newBurger = $(this).data("newsleep");
+        var newBurger = $(this).data("newBurger");
 
-        var newSleepState = {
-            sleepy: newSleep
+        var newBurgerAddition = {
+            name: newBurger
         };
-    })
 
-$.ajax("/burgers", {
-    type: "POST",
-    data: newCat
-}).then(
-    function () {
-        console.log("created new burger");
-        location.reload();
-    }
-);
+
+        $.ajax("/burgers" + id, {
+            type: "PUT",
+            data: newBurgerAddition
+        }).then(
+            function () {
+
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        var newBurger = {
+            name: $("#bu").val().trim(),
+        };
+
+
+        $.ajax("/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
 });

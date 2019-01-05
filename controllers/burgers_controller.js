5253548a15abf2d2ecf2bcdaaa6d8137ad7ec56f
unknown <jodimcarlson4@gmail.com>
2019-01-05 09:53:43 -0600

    fixing errors

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,15 +2,15 @@ var express = require("express");
 var router = express.Router();
 var burgers = require("../models/burger.js");
 
-router.get("/", function(request, response) {
+router.get("/", function (request, response) {
     burgers.selectAll(function (data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      response.render("index", hbsObject);
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        response.render("index", hbsObject);
     });
-  });
+});
 
 router.get("/api", function (request, response) {
     burgers.selectAll(function (data) {
@@ -22,11 +22,11 @@ router.post("/burgers", function (request, response) {
     burgers.insertOne([
         "Burger_name", "Devoured"
     ], [
-        request.body.Burger_name, request.body.Devoured],
+            request.body.Burger_name, request.body.Devoured],
         function (result) {
-        
-        response.json({id: result.insertId});
-    });
+
+            response.json({ id: result.insertId });
+        });
 });
 
 router.put("/burgers/:id", function (request, response) {
@@ -35,13 +35,13 @@ router.put("/burgers/:id", function (request, response) {
 
     burgers.update({
         Devoured: request.body.Devoured
-    }, condition, function(result){
+    }, condition, function (result) {
         if (result.changedRows == 0) {
             return response.status(404).end();
-        } else{
+        } else {
             response.status(200).end();
         }
     });
-    });
+});
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,24 @@
 var orm = require("../config/orm.js");
 
 var burgers = {
-    selectAll: function(cb) {
-      orm.selectAll("burgers", function(response) {
-        cb(response);
-      });
-    },
-    create: function(columns, values, cb) {
-        orm.insertOne("burgers", columns, values, function(response) {
-          cb(response);
-        });
-      },
-    
-      update: function(objColVals, cb) {
-        orm.updateOne("burgers", objColVals, function(response) {
-          cb(response);
-        });
-      }
-    };
-    
+  selectAll: function (cb) {
+    orm.selectAll("burgers", function (response) {
+      cb(response);
+    });
+  },
+  create: function (columns, values, cb) {
+    orm.insertOne("burgers", columns, values, function (response) {
+      cb(response);
+    });
+  },
+
+  update: function (objColVals, cb) {
+    orm.updateOne("burgers", objColVals, function (response) {
+      cb(response);
+    });
+  }
+};
+
 
 
 module.exports = burgers;
